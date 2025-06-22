28b015400431aa7d8dd6f83f0bf1d8def155730a
Melaniamat <melaniamatacena.97@gmail.com>
2025-06-22 16:50:36 +0200

    primo commit giornata

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,12 +4,20 @@
         <div class="p-5 text-center bg-body-tertiary">
             <div class="container py-5">
                 <h1 class="text-body-emphasis">Benvenuti nella homepage!</h1>
-                <p class="col-lg-8 mx-auto lead">
-                    Esplora i nostri prodotti oppure
-                </p>
-                <div><a href="{{ route('login') }}">Accedi</a> o
-                    <a href="{{ route('register') }}">Registrati</a>
-                </div>
+                @guest
+                    <p class="col-lg-8 mx-auto lead">
+                        Esplora i nostri prodotti oppure
+                    </p>
+                    <div><a href="{{ route('login') }}">Accedi</a> o
+                        <a href="{{ route('register') }}">Registrati</a>
+                    </div>
+                @else
+                    <div><a href="{{ route('create') }}">Crea un nuovo libro</a> os
+                        <a href="{{ route('index') }}">visualizza la lista</a>
+                    </div>
+
+                @endguest
+
 
 
             </div>
